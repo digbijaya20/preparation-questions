aa3348e7dc003f11a6950d4f60515d1f1a405a09
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2025-04-18 23:54:23 +0530

    closures

---
+++ js/closures.js
@@ -0,0 +1,33 @@
+//Closures is simply the ability of a function at the time of declaration to remember
+//  the references of variables and parameters on its current scope, on its parent function scope,
+//  on its parent's parent function scope until it reaches the global scope with the help of Scope Chain.
+//  Basically it is the Scope created when the function was declared.
+
+
+//🛍️ Real-Time Shopping Example – Discount Coupon System
+// Imagine you're building a shopping cart system.
+// You want to apply a discount coupon to the total price of items in the cart.
+// The discount coupon is a function that takes the total price and returns the discounted price.
+//
+// Here's how you can use closures to achieve this:
+function createCoupon(discountPercent) {
+    return function(price) {
+      return price - (price * discountPercent / 100);
+    };
+  }
+  
+//
+// Usage
+const blackFridayCoupon = createCoupon(30); // 30% off
+const studentCoupon = createCoupon(15);     // 15% off
+
+console.log(blackFridayCoupon(1000)); // 700
+console.log(studentCoupon(1000));     // 850
+
+// 🔍 How is this a closure?
+// createCoupon() returns a function that still remembers discountPercent, even after it has returned.
+// The inner function (price => ...) is the closure.
+// It's like each user walks away with their own personalized "discount calculator."
+
+
+// Closure	A function that "remembers"
--- react-js-interview-qs-by-company/Takeda-Pharmaceuticals.md
+++ react-js-interview-qs-by-company/Takeda-Pharmaceuticals.md
@@ -25,4 +25,10 @@ They help your app fail gracefully instead of breaking completely when something
 > As of v16.2.0, there's no way to turn a functional component into an error boundary. The **componentDidCatch()**  method works like a JavaScript **catch {}**  block, but for components. 
 > Only class components can be error boundaries. In practice, most of the time you’ll want to declare an error boundary component once and use it throughout your application.
 
-2. 
+2. What is Event Propagation?
+When an event occurs on a DOM element, that event does not entirely occur on that just one element. In the Bubbling Phase, the event bubbles up or it goes to its parent, to its grandparents, to its grandparent's parent until it reaches all the way to the window while in the Capturing Phase the event starts from the window down to the element that triggered the event or the event.target.
+
+
+3. What's the difference between event.preventDefault() and event.stopPropagation() methods?
+
+The event.preventDefault() method prevents the default behavior of an element. If used in a form element it prevents it from submitting. If used in an anchor element it prevents it from navigating. If used in a contextmenu it prevents it from showing or displaying. While the event.stopPropagation() method stops the propogation of an event or it stops the event from occurring in the bubbling or capturing phase.
