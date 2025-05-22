c4b11f6d5a87d2eaf75490d4ec4c5c44da8bd0cb
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2025-05-22 21:32:14 +0530

    added dynamic color change box

---
+++ js/dynamic-color-change-box.js
@@ -0,0 +1,19 @@
+
+
+const colorBox = document.getElementById('color-box');
+const colorList = document.getElementById('color-list');
+
+const colors = ['green','red', 'yellow', 'black'];
+
+
+colors.forEach(color =>{
+    const colorItem = document.createElement('div');
+    colorItem.classList.add('color-option-box');
+    colorItem.style.backgroundColor = color;
+
+    colorItem.addEventListener('click', ()=>{
+       colorBox.style.backgroundColor = color; 
+    })
+    colorList.appendChild(colorItem);
+
+})
