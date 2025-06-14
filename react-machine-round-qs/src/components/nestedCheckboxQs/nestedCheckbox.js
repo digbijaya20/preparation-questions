48f551ec8778134aed0db4be8b12904a06990a48
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2025-06-14 22:47:12 +0530

    Added nested checkbox checkbox funtionality

--- react-machine-round-qs/src/components/nestedCheckboxQs/nestedCheckbox.js
+++ react-machine-round-qs/src/components/nestedCheckboxQs/nestedCheckbox.js
@@ -2,12 +2,35 @@ import './nestedCheckbox.css';
 
 
 function Checkbox (props) {
+
+  function handleChange(e, node){
+    const isChecked = e.target.checked;
+    console.log(`Checkbox ${node.id} is now ${isChecked}`);
+    props.setChecked((pre) =>{
+      const newState = {...pre,[node.id]: isChecked}
+
+      const updateChildrenCheckbox = (node) =>{
+        node.children?.forEach((item) => {
+        newState[item.id]  = isChecked;
+        item.children &&  updateChildrenCheckbox(item);
+        }
+         
+        )
+      }
+      updateChildrenCheckbox(node);
+      return newState;
+    }
+  )
+  }
   return (
     <div>
       {props.checkboxData.map((node) => {
         return (
             <div key={node.id} className ='parent'>
-                <input type='checkbox' checked={props.checked[node.id] || false}/>
+                <input type='checkbox' 
+                checked={props.checked[node.id] || false}
+                onChange={(e) => handleChange(e, node)}
+                />
                 <label>{node.name}</label>
                 {node.children && 
                 <Checkbox 
